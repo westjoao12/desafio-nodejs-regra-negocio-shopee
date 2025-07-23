eee9fc1158da78170a9f49ff86163d5d119529ea
westjoao12 <westjoao12@gmail.com>
2025-07-23 06:40:02 +0100

    testando as funções

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ const myWhishList = [];
 console.log("Welcome to the your Shopee Cart!");
 
 //criando dois itens
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
-const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+const item1 = await createItem("Notebook Samsung", 1200.00, 5);
+const item2 = await createItem("Desktop Elit Book Pro", 3090.99, 67);
+const item3 = await createItem("Mouse Transform x98", 90.99, 50);
+const item4 = await createItem("Teclado RGB", 120.50, 67);
 
 // adicionei dois itens ao carrinho
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myCart, item2);
+await cartService.addItem(myCart, item3);
+await cartService.addItem(myCart, item4);
 
 await cartService.removeItem(myCart, item2);
 await cartService.removeItem(myCart, item2);
